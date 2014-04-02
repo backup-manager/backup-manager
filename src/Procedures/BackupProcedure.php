cfe986de9a01c29e912243c786e1c2a0522e4218
Shawn McCool <shawn@heybigname.com>
2014-04-02 16:22:23 +0200

    tidy up backupprocedure

--- src/Procedures/BackupProcedure.php
+++ src/Procedures/BackupProcedure.php
@@ -7,9 +7,8 @@ class BackupProcedure extends Procedure
         $localFilename = __DIR__.'/../../working/test.sql';
 
         $this->add($this->commandFactory->makeDumpDatabaseCommand($databaseName, $localFilename));
-        $command->execute();
-        die();
         $this->add($this->commandFactory->makeSaveFileCommand($storageName, $localFilename, $destinationPath));
+
         $this->execute();
     }
 }
