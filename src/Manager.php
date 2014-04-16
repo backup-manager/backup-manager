0bee43644bd36015f5b1d8618d9c5c6934b4fca9
Shawn McCool <shawn@heybigname.com>
2014-04-16 15:25:18 +0200

    refactor manager

--- src/Manager.php
+++ src/Manager.php
@@ -1,12 +1,8 @@
 <?php namespace BigName\BackupManager;
 
-use BigName\BackupManager\Compressors\GzipCompressor;
-use BigName\BackupManager\Config\Config;
-use BigName\BackupManager\Databases\DatabaseProvider;
-use BigName\BackupManager\Databases\MysqlDatabase;
-use BigName\BackupManager\Databases\PostgresqlDatabase;
+use BigName\BackupManager\Databases;
 use BigName\BackupManager\Filesystems;
-use BigName\BackupManager\Compressors\CompressorProvider;
+use BigName\BackupManager\Compressors;
 use BigName\BackupManager\Procedures\Sequence;
 use BigName\BackupManager\Procedures;
 use BigName\BackupManager\ShellProcessing\ShellProcessor;
@@ -19,35 +15,28 @@ use Symfony\Component\Process\Process;
 class Manager
 {
     /**
-     * @var Config\Config
+     * @var Filesystems\FilesystemProvider
      */
-    protected $storage;
+    private $filesystems;
     /**
-     * @var Config\Config
+     * @var Databases\DatabaseProvider
      */
-    protected $database;
+    private $databases;
     /**
-     * @var \BigName\BackupManager\Filesystems\FilesystemProvider
+     * @var Compressors\CompressorProvider
      */
-    protected $filesystems;
-    /**
-     * @var \BigName\BackupManager\Databases\DatabaseProvider
-     */
-    protected $databases;
-    /**
-     * @var \BigName\BackupManager\Compressors\CompressorProvider
-     */
-    protected $compressors;
+    private $compressors;
 
     /**
-     * @param $storage
-     * @param $database
+     * @param Filesystems\FilesystemProvider $filesystems
+     * @param Databases\DatabaseProvider $databases
+     * @param Compressors\CompressorProvider $compressors
      */
-    public function __construct($storage, $database)
+    public function __construct(Filesystems\FilesystemProvider $filesystems, Databases\DatabaseProvider $databases, Compressors\CompressorProvider $compressors)
     {
-
-        $this->storage = new Config($storage);
-        $this->database = new Config($database);
+        $this->filesystems = $filesystems;
+        $this->databases = $databases;
+        $this->compressors = $compressors;
     }
 
     /**
@@ -56,9 +45,9 @@ class Manager
     public function makeBackup()
     {
         return new Procedures\BackupProcedure(
-            $this->getFilesystemProvider(),
-            $this->getDatabaseProvider(),
-            $this->getCompressorProvider(),
+            $this->filesystems,
+            $this->databases,
+            $this->compressors,
             $this->getShellProcessor(),
             new Sequence
         );
@@ -70,9 +59,9 @@ class Manager
     public function makeRestore()
     {
         return new Procedures\RestoreProcedure(
-            $this->getFilesystemProvider(),
-            $this->getDatabaseProvider(),
-            $this->getCompressorProvider(),
+            $this->filesystems,
+            $this->databases,
+            $this->compressors,
             $this->getShellProcessor(),
             new Sequence
         );
@@ -84,50 +73,5 @@ class Manager
     protected function getShellProcessor()
     {
         return new ShellProcessor(new Process(''));
-    }
-
-    /**
-     * @return FilesystemProvider
-     */
-    protected function getFilesystemProvider()
-    {
-        if ( ! $this->filesystems) {
-            $provider = new Filesystems\FilesystemProvider($this->storage);
-            $provider->add(new Filesystems\Awss3Filesystem);
-            $provider->add(new Filesystems\DropboxFilesystem);
-            $provider->add(new Filesystems\FtpFilesystem);
-            $provider->add(new Filesystems\LocalFilesystem);
-            $provider->add(new Filesystems\RackspaceFilesystem);
-            $provider->add(new Filesystems\SftpFilesystem);
-            $this->filesystems = $provider;
-        }
-        return $this->filesystems;
-    }
-
-    /**
-     * @return CompressorProvider
-     */
-    protected function getCompressorProvider()
-    {
-        if ( ! $this->compressors) {
-            $provider = new CompressorProvider;
-            $provider->add(new GzipCompressor);
-            $this->compressors = $provider;
-        }
-        return $this->compressors;
-    }
-
-    /**
-     * @return DatabaseProvider
-     */
-    protected function getDatabaseProvider()
-    {
-        if ( ! $this->databases) {
-            $provider = new DatabaseProvider($this->database);
-            $provider->add(new MysqlDatabase);
-            $provider->add(new PostgresqlDatabase);
-            $this->databases = $provider;
-        }
-        return $this->databases;
     }
 } 
