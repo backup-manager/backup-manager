de1de13c0c44c27fa36da6afd99b992b0c662071
Mitchell van Wijngaarden <mitchell@kooding.nl>
2014-06-12 16:55:13 +0200

    Add root to list

--- src/Integrations/Laravel/DbListCommand.php
+++ src/Integrations/Laravel/DbListCommand.php
@@ -140,7 +140,7 @@ class DbListCommand extends BaseCommand
         $root = $this->filesystems->getConfig($this->option('source'), 'root');
         $this->info('Just to be sure...');
         $this->info(sprintf('Do you want to list files from <comment>%s</comment> on <comment>%s</comment>',
-            $this->option('path'),
+            $root.$this->option('path'),
             $this->option('source')
         ));
         $this->line('');
