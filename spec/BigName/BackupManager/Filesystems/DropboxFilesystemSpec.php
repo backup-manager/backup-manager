f239523e021569024632f46186495437fcc211bc
Graham Campbell <graham@mineuk.com>
2015-01-25 20:07:09 +0000

    Test

--- spec/BigName/BackupManager/Filesystems/DropboxFilesystemSpec.php
+++ spec/BigName/BackupManager/Filesystems/DropboxFilesystemSpec.php
@@ -26,7 +26,7 @@ class DropboxFilesystemSpec extends ObjectBehavior
     function it_should_provide_an_instance_of_a_dropbox_filesystem()
     {
         $this->get($this->getConfig())->getAdapter()
-            ->shouldHaveType('League\Flysystem\Adapter\Dropbox');
+            ->shouldHaveType('League\Flysystem\Dropbox\DropboxAdapter');
     }
 
     function getConfig()
