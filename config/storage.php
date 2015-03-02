e22d323f2fbef057359eb24e6b9b784ebc9cb52e
Adam Klein <asklein@gmail.com>
2015-03-02 09:03:29 -0800

    1. putting back file

---
+++ config/storage.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'local' => [
+        'type' => 'Local',
+        'root' => base_path('/storage/app/backups'),
+    ],
+    's3' => [
+        'type' => 'AwsS3',
+        'key'    => '',
+        'secret' => '//',
+        'region' => 'us-east-1',
+        'bucket' => '',
+        'root'   => '',
+    ],
+    'rackspace' => [
+        'type' => 'Rackspace',
+        'username' => '',
+        'key' => '',
+        'container' => '',
+        'zone' => '',
+        'endpoint' => 'https://identity.api.rackspacecloud.com/v2.0/',
+        'root' => '',
+    ],
+    'dropbox' => [
+        'type' => 'Dropbox',
+        'token' => '',
+        'key' => '',
+        'secret' => '',
+        'app' => '',
+        'root' => '',
+    ],
+    'ftp' => [
+        'type' => 'Ftp',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 21,
+        'passive' => true,
+        'ssl' => true,
+        'timeout' => 30,
+        'root' => '',
+    ],
+    'sftp' => [
+        'type' => 'Sftp',
+        'host' => '',
+        'username' => '',
+        'password' => '',
+        'port' => 21,
+        'timeout' => 10,
+        'privateKey' => '',
+        'root' => '',
+    ],
+];
